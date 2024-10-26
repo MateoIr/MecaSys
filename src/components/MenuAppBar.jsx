75496d59f9f301cf69ec847284086954e85b7acd
MateoIr <mateoiriso@gmail.com>
2024-10-26 15:52:58 -0300

    r

--- src/components/MenuAppBar.jsx
+++ src/components/MenuAppBar.jsx
@@ -6,18 +6,23 @@ import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import logo2 from "../img/logo2.png";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export default function MenuAppBar() {
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
   };
 
+  const navigate = useNavigate();
   return (
     <Box sx={{ flexGrow: 1, width: "100vw" }}>
       <AppBar position="static" sx={{ backgroundColor: "grey" }}>
-        <Toolbar>
+        <Toolbar on>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Box sx={{ width: "20px", height: "60px" }}>
+            <Box
+              sx={{ width: "20px", height: "60px", cursor: "pointer" }}
+              onClick={() => navigate("/")}
+            >
               <img src={logo2} alt="lgo" height="60px" />
             </Box>
           </Typography>
--- src/pages/CreateTrun.jsx
+++ src/pages/CreateTrun.jsx
@@ -1,5 +1,58 @@
+import {
+  Box,
+  FormControl,
+  Grid,
+  InputLabel,
+  List,
+  MenuItem,
+  Select,
+  TextField,
+} from "@mui/material";
+import MenuAppBar from "../components/MenuAppBar";
+
 const CreateTrun = () => {
-  return <div>Crear turno</div>;
+  return (
+    <Box height={"100vh"}>
+      <MenuAppBar />
+      <Box
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "red",
+        }}
+      >
+        <Grid container sx={{ margin: 1, width: "80%" }}>
+          <Grid xs={6} md={3}>
+            Cliente:
+          </Grid>
+          <Grid xs={6} md={3}>
+            <FormControl variant="filled" sx={{ m: 1, minWidth: 120 }}>
+              <Select
+                labelId="demo-simple-select-filled-label"
+                id="demo-simple-select-filled"
+                // value={age}
+                // onChange={handleChange}
+              >
+                <MenuItem value="">
+                  <em>None</em>
+                </MenuItem>
+                <MenuItem value={10}>Ten</MenuItem>
+                <MenuItem value={20}>Twenty</MenuItem>
+                <MenuItem value={30}>Thirty</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+          <Grid xs={6} md={3}>
+            Nombres:
+          </Grid>
+          <Grid xs={6} md={3}>
+            <TextField id="filled-basic" label="Filled" variant="filled" />
+          </Grid>
+        </Grid>
+      </Box>
+    </Box>
+  );
 };
 
 export default CreateTrun;
